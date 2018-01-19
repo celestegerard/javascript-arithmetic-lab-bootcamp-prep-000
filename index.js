9c031d14135015af02cc5c6bed29c067bcb8a44a
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-19 16:16:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var number = 7
 
-function add(number, 3) {
+function add(a, b) {
   number += 3
 }
 
-function subtract(number,3){
+function subtract(a, b){
   number -= 3
 }
 
-function multiply(number, 3){
+function multiply(a, b){
   number *= 3
 }
 
-function divide(number, 3){
+function divide(a, b){
   number /= 3
 }
